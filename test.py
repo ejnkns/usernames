ae6f88e0ffbde8ad4e6a1c2ececf107eecc2093c
Elliot Jenkins <elliot@linux.com>
2017-07-21 17:41:32 +1000

    update test.py

--- test.py
+++ test.py
@@ -1,5 +1,10 @@
 import requests
 
+# for checking email addresses
+import dns.resolver
+import socket
+import smtplib
+
 def checkReddit(username):
     # Reddit has an api request limit so might need to change this to just
     # parse the regular html page at www.reddit.com/user/[username]
@@ -23,4 +28,42 @@ def checkTwitter(username):
     return(site.json()['valid'])
 #print(checkTwitter('ejnkns'))
 
+def checkGoogle(username):
+    # to do this we check if the gmail account exists using dns
+    # every google account has a gmail
 
+    records = dns.resolver.query('gmail.com', 'MX')
+    mxRecord = str(records[0].exchange)
+
+    # SMTP lib setup (use debug level for full output)
+    server = smtplib.SMTP()
+    server.set_debuglevel(0)
+
+    # SMTP Conversation
+    server.connect(mxRecord)
+    server.helo(server.local_hostname)
+    server.mail('findallusernames@gmail.com')
+    code, message = server.rcpt(str(username + '@gmail.com'))
+    server.quit()
+
+    # Assume 250 as Success
+    if code == 250:
+        return(True)
+    else:
+        return(False)
+#these always time out ...
+#print(checkGoogle('elliotjenkins3'))
+#print(checkGoogle('apirgfjh'))
+
+def checkInstagram(username):
+    userPage = requests.head('http://instagram.com/' + username)
+    if userPage.status_code == 200:
+        return(True)
+    else:
+        return(False)
+
+#print(checkInstagram('aldkfjthaf'))
+#print(checkInstagram('johndoe'))
+
+
+def 
